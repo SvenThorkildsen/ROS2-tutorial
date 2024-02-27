bcceb94d19b9d4d3113d847d8b577e5b05c96c50
sven <sven@cleanseasolutions.no>
2024-02-27 14:08:00 +0100

    Add launch file for spawning robot and environment in Gazebo

--- build/my_robot_bringup/colcon_command_prefix_build.sh
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -1,4 +1,5 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/sven/ros2_ws/install/my_robot_description/share/my_robot_description/package.sh"
 . "/home/sven/ros2_ws/install/my_robot_interfaces/share/my_robot_interfaces/package.sh"
 . "/home/sven/ros2_ws/install/my_cpp_pkg/share/my_cpp_pkg/package.sh"
 . "/home/sven/ros2_ws/install/my_py_pkg/share/my_py_pkg/package.sh"
