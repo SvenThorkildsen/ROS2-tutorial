a08c03021acaff2b38a740e835e9fa4863fd8901
sven <sven@cleanseasolutions.no>
2024-03-06 16:24:51 +0100

    Add executors

---
+++ build/executors_cpp/CMakeFiles/multi_threaded_executor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multi_threaded_executor.dir/src/multi_threaded_executor.cpp.o"
+  "CMakeFiles/multi_threaded_executor.dir/src/multi_threaded_executor.cpp.o.d"
+  "multi_threaded_executor"
+  "multi_threaded_executor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multi_threaded_executor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/executors_cpp/CMakeFiles/single_threaded_executor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/single_threaded_executor.dir/src/single_threaded_executor.cpp.o"
+  "CMakeFiles/single_threaded_executor.dir/src/single_threaded_executor.cpp.o.d"
+  "single_threaded_executor"
+  "single_threaded_executor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/single_threaded_executor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
